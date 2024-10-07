4351b8728d62e08624fb5da10ebddb51e5a4e8e0
Anthony Garrett <tony.garrett@gmail.com>
2024-10-07 18:54:02 -0400

    Fundamentals 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,77 @@
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstNamePerson = "Jonas";
+
+// // Variable name conventions
+// let jonas_matilda = "JM";
+// let $function = 27;
+
+// let person = "jonas";
+// let PI = 3.1415;
+
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+
+// let job1 = "programmer";
+// let job2 = "teacher";
+
+// console.log(myCurrentJob);
+
+// let country = "United States";
+// let continent = "North America";
+// let population = 333;
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+// console.log(typeof true);
+
+// javascriptIsFun = "YES!";
+// console.log(typeof javascriptIsFun);
+// console.log(javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// let age = 30;
+// age = 31;
+
+// const birthYear = 1979;
+
+// const job = "Freelance";
+// const now = 2037;
+// const ageTony = now - 1979;
+// const ageSarah = now - 2018;
+
+// console.log(ageTony, ageSarah);
+// console.log(ageTony * 2, ageTony / 2, 2 ** 3);
+
+// const firstName = "Tony";
+// const lastName = "Garrett";
+// console.log(firstName + " " + lastName);
+
+// let x = 10 + 5;
+// x += 10;
+// x *= 4;
+// x++;
+// x--;
+// x--;
+// console.log(x);
+
+// console.log(ageTony > ageSarah);
+// console.log(ageSarah >= 18);
+
+// const isFullAge = ageSarah >= 18;
+
+// console.log(now - 1991 > now - 2018);
