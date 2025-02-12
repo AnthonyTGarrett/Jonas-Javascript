faa90b539bfd72c355470a17f254de329d675757
Tony Garrett <tony.garrett@gmail.com>
2025-02-12 18:22:01 -0500

    Finished the 3rd challenge

--- 09-Data-Structures-Operators/starter/challenges.js
+++ 09-Data-Structures-Operators/starter/challenges.js
@@ -1,5 +1,35 @@
 'use strict';
 
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+const events = new Array(...new Set(gameEvents.values()));
+console.log(events);
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+for (const event of gameEvents) {
+  console.log(
+    `[${event[0] < 45 ? 'FIRST HALF' : 'SECOND HALF'}] ${event[0]}: ${event[1]}`
+  );
+}
+
 // const game = {
 //   team1: 'Bayern Munich',
 //   team2: 'Borrussia Dortmund',
