2bb6150517e37cb69633a0b6420344fdc9b32b1e
Tony Garrett <tony.garrett@gmail.com>
2025-02-23 09:39:51 -0500

    Working on some closure lectures

---
+++ 10-Functions/starter/challenge.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// const poll = {
+//   question: 'What is your favourite programming language?',
+
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+
+//   // This generates [0, 0, 0, 0]. More in the next section 😃
+//   answers: new Array(4).fill(0),
+
+//   registerNewAnswer() {
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+//       )
+//     );
+
+//     // Weird as shit way of doing this
+//     // typeof answer === 'number' && answer < this.answers.length && this.answers[answer]++;
+//     if (isNaN(answer) || answer < 0 || answer > 3) {
+//       console.log('ERR: Please enter a valid number.');
+//       return;
+//     }
+//     this.answers[Number(answer)] += 1;
+
+//     this.displayResults('array');
+//     this.displayResults('string');
+//   },
+
+//   displayResults(type = 'array') {
+//     if (type == 'array') {
+//       console.log(this.answers);
+//     } else {
+//       console.log(`Poll results are ${this.answers.join(', ')}`);
+//     }
+//   },
+// };
+
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+// poll.displayResults.call({ answers: [5, 2, 3] });
--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,227 @@
 'use strict';
+
+////   CLOSURES
+//////////////////////
+// const secureBooking = function () {
+//   let passengerCount = 0;
+
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+g();
+f();
+
+// const runOnce = function () {
+//   console.log('This will never run again');
+// };
+// runOnce();
+
+// (function () {
+//   console.log('This will never run again');
+//   const isPrivate = 23;
+// })();
+// (() => {
+//   console.log('This will never run again');
+// })();
+
+// const lufthansa = {
+//   airline: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
+
+// lufthansa.book(348, 'Anthony Garrett');
+// lufthansa.book(456, 'Anthony Spades');
+
+// const eurowings = {
+//   airline: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
+
+// // This is a regular function and not a method anymore
+// // That means no this keyword and won't work
+// const book = lufthansa.book;
+// // book(23, 'Sarah Williams');
+
+// // Manually setting the this keyword for the call
+// book.call(eurowings, 23, 'Sarah Williams');
+// console.log(eurowings);
+
+// book.call(lufthansa, 239, 'Mary Cooper');
+// console.log(lufthansa);
+
+// const swiss = {
+//   airline: 'Swiss Air Lines',
+//   iataCode: 'LX',
+//   bookings: [],
+// };
+
+// book.call(swiss, 583, 'Mary Cooper');
+// console.log(swiss);
+
+// // Apply method uses an array instead
+// const flightData = [583, 'George Cooper'];
+// book.apply(swiss, flightData);
+// console.log(swiss);
+
+// // Modern and better. Don't use the apply ever really
+// book.call(swiss, ...flightData);
+
+// // Bind Method
+// // book.call(eurowings, 23, 'Sarah Williams');
+
+// const bookEW = book.bind(eurowings);
+// const bookLH = book.bind(eurowings);
+// const bookLX = book.bind(swiss);
+
+// bookEW(23, 'Steven Williams');
+
+// // Also able to bind the arguments too
+// // This is called partial application
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Tony Garrett');
+// bookEW23('Tony Barrett');
+// console.log(eurowings);
+
+// // With Event Listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   console.log(this);
+
+//   this.planes++;
+//   console.log(this.planes);
+// };
+
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// // More partial applications
+// const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.23);
+// console.log(addVAT(250));
+
+// const addTaxRate = function (rate) {
+//   return function (value) {
+//     return value + value * rate;
+//   };
+// };
+
+// const addVAT2 = addTaxRate(0.23);
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greet = greeting => name => console.log(`${greeting} ${name}`);
+
+// const greeterHey = greet('Hey');
+// greeterHey('Tony');
+// greeterHey('Anthony');
+
+// greet('Hello')('Tony');
+
+// const oneWord = function (str) {
+//   return str.replace(/ /g, '').toLowerCase();
+// };
+
+// const upperFirstWord = function (str) {
+//   const [first, ...others] = str.split(' ');
+//   return [first.toUpperCase(), ...others].join(' ');
+// };
+
+// const transformer = function (str, fn) {
+//   console.log(`Original string: ${str}`);
+//   console.log(`Transformed string: ${fn(str)}`);
+
+//   console.log(`Transformed by: ${fn.name}`);
+// };
+
+// transformer('Javascript is the best!', upperFirstWord);
+// transformer('Javascript is the best!', oneWord);
+
+// const highFive = function () {
+//   console.log('👏');
+// };
+
+// document.body.addEventListener('click', highFive);
+
+// ['Tony', 'Anthony', 'Wesley'].forEach(highFive);
+
+// const bookings = [];
+
+// const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+
+// // How to skip a default parameter
+// createBooking('LH123', undefined, 850);
+
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 214578265322,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 214578265322) {
+//     alert('Checked In');
+//   } else {
+//     alert('Wrong passport!');
+//   }
+// };
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000000000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
